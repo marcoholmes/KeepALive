19aca2b2b328714e2016df41452f5b0767bb30cc
F <f>
2018-03-21 13:52:43 +0100

    ciao

--- KeepAlive/App_Start/Startup.Auth.cs
+++ KeepAlive/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using KeepAlive.Models;
+using System.Web.Mvc;
 
 namespace KeepAlive
 {
@@ -15,9 +16,11 @@ namespace KeepAlive
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
-            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+            //app.CreatePerOwinContext(ApplicationDbContext.Create);
+            //app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext(DependencyResolver.Current.GetService<ApplicationUserManager>);
+            app.CreatePerOwinContext(DependencyResolver.Current.GetService<ApplicationSignInManager>);
+            //app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
