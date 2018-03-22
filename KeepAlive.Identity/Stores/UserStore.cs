ffd93aeb9ea89989cf5de4b3cb8a5378389a0631
F <f>
2018-03-22 19:18:16 +0100

    userstore modified

--- KeepAlive.Identity/Stores/UserStore.cs
+++ KeepAlive.Identity/Stores/UserStore.cs
@@ -7,11 +7,14 @@ using System.Web;
 
 namespace KeepAlive.Identity
 {
-    public class UserStore : IUserStore<IdentityUser, int>
+    public class UserStore : IUserStore<IdentityUser, int>,
+                             IUserPasswordStore<IdentityUser, int>,
+                             IUserEmailStore<IdentityUser, int>
+                                
     {
         public Task CreateAsync(IdentityUser user)
         {
-            throw new NotImplementedException();
+            return Task.CompletedTask;
         }
 
         public Task DeleteAsync(IdentityUser user)
@@ -29,18 +32,58 @@ namespace KeepAlive.Identity
 
         public Task<IdentityUser> FindByIdAsync(int userId)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(new IdentityUser(1));
         }
 
         public Task<IdentityUser> FindByNameAsync(string userName)
         {
             IdentityUser user = new IdentityUser(1);
-            return Task.FromResult(user);
+            return Task.FromResult(default(IdentityUser));
         }
 
         public Task UpdateAsync(IdentityUser user)
         {
             throw new NotImplementedException();
+        }
+
+        public Task SetPasswordHashAsync(IdentityUser user, string passwordHash)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<string> GetPasswordHashAsync(IdentityUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> HasPasswordAsync(IdentityUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetEmailAsync(IdentityUser user, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetEmailAsync(IdentityUser user)
+        {
+            return Task.FromResult("k@k.it");
+        }
+
+        public Task<bool> GetEmailConfirmedAsync(IdentityUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetEmailConfirmedAsync(IdentityUser user, bool confirmed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityUser> FindByEmailAsync(string email)
+        {
+            return Task.FromResult(default(IdentityUser));
         }
     }
 }
