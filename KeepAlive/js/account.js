15ec3b02ea0a548e0fdb92fa15ee424945a634da
F <f>
2018-05-15 18:11:18 +0200

    user recovery continuing progress

--- KeepAlive/js/account.js
+++ KeepAlive/js/account.js
@@ -1,21 +1,9 @@
-﻿//<a href="/Index/Stuff" id="action">show stuff</a>
-
-//$(function () {
-//    $('#userRecovery').click(function () {
-//        alert("preso!");
-//        //$.ajax({
-//        //    url: "userRecovery/account",
-//        //    type: "get",
-//        //    async: true,
-//        //    success: function (html) {
-//        //        $('#recovery').html(html);
-//        //    }
-//        //    })
-//    });
-//});
-
+﻿
 $(function () {
-    $(document).on("click", "#userRecovery", function () {
+
+    var $container = $('#account');
+
+    $container.on("click", "#userRecovery", function () {
         $.ajax({
             url: "userRecovery/account",
             type: "get",
@@ -26,18 +14,3 @@ $(function () {
         })
         });
 });
-
-//$(function () {
-//    $('#user-recovery').click(function (e) {
-//        e.preventDefault();
-//        var url = $(this).attr('href');
-//        $.ajax({
-//            url: "user-recovery",
-//            //beforeSend: OnBeginWork,
-//            //complete: OnCompleteWork,
-//            success: function (html) {
-//                $('#recovery').html(html);
-//            }
-//        });
-//    });
-//});
